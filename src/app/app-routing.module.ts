91bfdaf889712fe86fb71830bb3a83a8558a1895
Mansi Kagda <mansikagda18@gmail.com>
2023-09-22 22:38:48 +0530

    set up for home component and creating navbar and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { EventComponent } from './event/event.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Default route
+  { path: 'event', component: EventComponent }, // Define routes for other components
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,70 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import {MatInputModule} from "@angular/material/input"
+import {MatSelectModule} from "@angular/material/select"
+import {MatAutocompleteModule} from "@angular/material/autocomplete"
+import {MatToolbarModule} from "@angular/material/toolbar"
+import {MatMenuModule} from "@angular/material/menu"
+import {MatIconModule} from "@angular/material/icon"
+import {MatButtonModule} from "@angular/material/button"
+import {MatBadgeModule} from "@angular/material/badge"
+import {MatSidenavModule} from "@angular/material/sidenav"
+import {MatListModule} from "@angular/material/list"
+import {MatCardModule} from "@angular/material/card"
+import {MatSliderModule} from "@angular/material/slider"
+import {MatTableModule} from "@angular/material/table"
+import {MatPaginatorModule} from "@angular/material/paginator"
+import {MatSortModule} from "@angular/material/sort"
+import {MatDatepickerModule} from "@angular/material/datepicker"
+import {MatNativeDateModule} from "@angular/material/core"
+import {MatRadioModule} from "@angular/material/radio"
+import {MatCheckboxModule} from "@angular/material/checkbox"
+import {MatDialogModule} from "@angular/material/dialog";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EventComponent } from './event/event.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ContactComponent,
+    NavbarComponent,
+    FooterComponent,
+    EventComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatInputModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatSliderModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
