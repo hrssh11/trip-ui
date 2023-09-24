d96906829e0b8bb5a7cc7e062a7a13a40fc1f7ad
Harsh Gupta <harsh.g@73strings.com>
2023-09-24 17:06:17 +0530

    handel event navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { CommonserviceService } from '../home/commonService/commonservice.service';
+import { switchScan } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  isHome: boolean = true;
+  isEvent: boolean = false
+  isContact: boolean = false
+  isLogin: boolean = false
+
+  constructor(public commonService: CommonserviceService) {
+
+  }
+
+  isEventClick(event) {
+
+    let typeEvent = event.target.innerHTML
+
+    switch (typeEvent) {
+      case 'Home':
+        this.isHome = true;
+        this.isContact = false;
+        this.isLogin = false;
+        this.isEvent = false;
+        break;
+      case 'Event':
+        this.isEvent = true;
+        this.isHome = false;
+        this.isContact = false;
+        this.isLogin = false;
+        break;
+      case 'Contact':
+        this.isContact = true;
+        this.isHome = false;
+        this.isEvent = false;
+        this.isLogin = false;
+        break;
+      case 'Login':
+        this.isLogin = true;
+        this.isHome = false;
+        this.isEvent = false;
+        this.isContact = false;
+        break;
+    }
+
+  }
+
+
 
 }
