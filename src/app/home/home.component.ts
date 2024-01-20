6cf6507ee6120782375bb9cb6dde729336d05306
Harsh Gupta <harsh.g@73strings.com>
2024-01-20 21:33:00 +0530

    home page ui fixes latest with drible figma

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, HostListener, OnInit } from '@angular/core';
-import { VideoStateService } from '../servicess/videostate.service';
 
 @Component({
   selector: 'app-home',
@@ -8,28 +7,10 @@ import { VideoStateService } from '../servicess/videostate.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private videoStateService: VideoStateService) {
+  constructor() {
 
   }
   ngOnInit(): void {
-
-    const video = document.getElementById('bg-video') as HTMLVideoElement;
-    // Check if there's a saved playback time in local storage
-    const savedTime = localStorage.getItem('videoPlaybackTime');
-    if (savedTime) {
-      video.currentTime = parseFloat(savedTime);
-    }
-
-    // Save the current playback time to local storage when the page is unloaded
-    window.addEventListener('beforeunload', () => {
-      this.videoStateService.setPlaybackTime(video.currentTime);
-    });
-
-    // Ensure that the video is muted for autoplay to work
-    video.muted = true;
-
-    // Attempt to play the video
-    video.play().catch((error) => console.error(error));
 
   }
   }
--- src/app/servicess/videostate.service.ts
+++ src/app/servicess/videostate.service.ts
@@ -17,3 +17,26 @@ export class VideoStateService {
     localStorage.setItem('videoPlaybackTime', String(time));
   }
 }
+
+
+// ngOnInit(): void {
+
+//   const video = document.getElementById('bg-video') as HTMLVideoElement;
+//   // Check if there's a saved playback time in local storage
+//   const savedTime = localStorage.getItem('videoPlaybackTime');
+//   if (savedTime) {
+//     video.currentTime = parseFloat(savedTime);
+//   }
+
+//   // Save the current playback time to local storage when the page is unloaded
+//   window.addEventListener('beforeunload', () => {
+//     this.videoStateService.setPlaybackTime(video.currentTime);
+//   });
+
+//   // Ensure that the video is muted for autoplay to work
+//   video.muted = true;
+
+//   // Attempt to play the video
+//   video.play().catch((error) => console.error(error));
+
+// }
