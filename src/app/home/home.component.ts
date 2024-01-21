1dc36271dd25ec0b867daec0c3faca252bfea065
Harsh Gupta <harsh.g@73strings.com>
2024-01-21 15:48:15 +0530

    categories ui in home ui

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,12 +7,44 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  categories: any[] = [
+    {
+      category: 'family',
+      src: 'assets/images/familytrip.jpeg',
+      title: 'Family Trip'
+    },
+    {
+      category: 'group',
+      src: 'assets/images/groptrip.jpeg',
+      title: 'Group Trip'
+    },
+    {
+      category: 'hiking',
+      src: 'assets/images/hiking.jpeg',
+      title: 'Hiking Trip'
+    },
+    {
+      category: 'road',
+      src: 'assets/images/roadtrip.jpeg',
+      title: 'Road Trip'
+    },
+    {
+      category: 'cruises',
+      src: 'assets/images/cruises.jpeg',
+      title: 'Cruises Trip'
+    },
+    {
+      category: 'cultural',
+      src: 'assets/images/culturaltrip.jpeg',
+      title: 'Cultural Trip'
+    },
+
+  ]
   constructor() {
 
   }
   ngOnInit(): void {
-
   }
-  }
+}
 
 
