2d2183f475ac18bd069d692346deb1a153467aca
Harsh Gupta <harsh.g@73strings.com>
2024-01-23 22:03:51 +0530

    minor fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'trip-ui';
+
+  isLoginRoute:boolean
+  constructor(
+    private router: Router,
+    ) {
+      this.router.events.subscribe((events:any)=>{
+        if(events instanceof NavigationEnd){
+          if(events.url == '/login'){
+            this.isLoginRoute = true;
+          }else{
+            this.isLoginRoute = false;
+          }
+        }
+      })
+  }
 }
