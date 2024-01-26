7cb575dc1023dc438caf5ff41aa807a694257965
Harsh Gupta <harsh.g@73strings.com>
2024-01-26 18:09:17 +0530

    add validation in phone number

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,24 +10,27 @@ import { ToastrService } from 'ngx-toastr';
 export class LoginComponent implements OnInit {
 
   signupForm: FormGroup;
+  vendorRegistorForm: FormGroup;
   title: string = 'Sign In'
   isLogin: boolean = false;
   isMybizz: boolean = false;
   isSinup: boolean = false;
+  isProc
   constructor(private userService: UserService, private fb: FormBuilder , private toastr: ToastrService) { }
   ngOnInit(): void {
     this.signupForm = this.fb.group({
       username: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(4)]]
     });
-  }
-
-  onLogin() {
-    if (this.signupForm.valid) {
-      this.userService.onLogin(this.signupForm.value).subscribe(res => {
-        console.log('res', res);
-      })
-    }
+    this.vendorRegistorForm = this.fb.group({
+      firstname: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
+      businessName: ['', [Validators.required]],
+      phoneNo: ['', [ Validators.required ,Validators.maxLength(10) , Validators.pattern('^[0-9]*$'),]],
+      email: ['', [Validators.required, Validators.email]],
+      userRole: 2,
+      password: ['', [Validators.required, Validators.minLength(4)]]
+    });
   }
   onMybizRegister(titleValue: string) {
     this.title = titleValue;
@@ -40,10 +43,23 @@ export class LoginComponent implements OnInit {
     this.isSinup = true;
   }
   backToLogin(titleValue: string) {
-    this.toastr.success('Hello world!', 'Toastr fun!');
+    // this.toastr.success('Hello world!', 'Toastr fun!');
     this.title = titleValue;
     this.isLogin = false;
     this.isMybizz= false;
     this.isSinup = false;
   }
+
+  onLogin() {
+    if (this.signupForm.valid) {
+      this.userService.onLogin(this.signupForm.value).subscribe(res => {
+        console.log('res', res);
+      })
+    }
+  }
+ onVendorRegistor(){
+    if(this.vendorRegistorForm.valid){
+   console.log(this.vendorRegistorForm)
+    }
+ }
 }
